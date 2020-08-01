8772d346fb3516226284182ba44786a1f93e3737
Данил Горев <ketov-x@yandex.ru>
2020-08-01 18:28:07 +0300

    * Minor fixes.

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import setup, find_packages
 from os.path import join, dirname
 
+import configger
 
 setup(
     name='py-configger',
-    version='a0.1.0',
+    version=configger.__version__,
     packages=find_packages(),
     long_description=open(join(dirname(__file__), 'README.md')).read(),
+    author="@PaperDevil",
+    author_email="ketov-x@yandex.ru"
 )
