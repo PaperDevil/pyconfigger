bbcfd4e58f274f1ee2d7905b5ee98ef46acde037
ketovx <ketov-x@yandex.ru>
2020-08-19 23:08:46 +0300

    UPD: changed version to 0.1.2a

--- configger/__init__.py
+++ configger/__init__.py
@@ -1,3 +1,3 @@
-__version__ = "0.1.a1"  # https://www.python.org/dev/peps/pep-0386/
+__version__ = "0.1.2a"  # https://www.python.org/dev/peps/pep-0386/
 
 from configger.config import BasicConfig, SingleConfig
